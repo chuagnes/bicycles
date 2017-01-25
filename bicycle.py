d70421afa2df035a18a3e74620a02b9dc742a327
chuagnes <agnes.p.chu@gmail.com>
2017-01-25 04:00:08 +0000

    added wheels and frames

--- bicycle.py
+++ bicycle.py
@@ -1,31 +1,98 @@
-class bicycle(object):
-    def __init__(self, name, weight, prodcost):
+class Bicycle(object):
+    def __init__(self, name, wheel1, wheel2, frame, manufacturer):
         self.name = name
-        self.weight = weight
-        self.prodcost = prodcost
-    
-class bikeshop(object):
+        self.frame = frame
+        self.price = 0
+        self.manufacturer = manufacturer #does this have to be an input?
+        if wheel1.wheel_type == wheel2.wheel_type:
+            self.wheel1 = wheel1
+            self.wheel2 = wheel2 
+        #how to return error if the two wheels aren't the same type?
+        self.prod_cost = wheel1.prod_cost + wheel2.prod_cost + frame.prod_cost
+        self.weight = wheel1.weight + wheel2.weight + frame.weight  
+        
+
+class Bikeshop(object):
     def __init__(self, name, inventory, margin, profit = 0):
         self.name = name
         self.inventory = inventory
         self.margin = margin
         self.profit = profit
+        
+        for bike in self.inventory:
+            bike.price = bike.prod_cost * (1 + self.margin)
     
-class customers(object):
+    def afford_bikes(self, customer):
+        bike_list = []
+        for b in self.inventory:
+            if b.price <= customer.budget:
+                bike_list.append(b.name)
+        return bike_list
+    
+    def stock_inventory(self, new_inventory):
+        for new_bike in new_inventory:
+            for bike in self.inventory:
+                if new_bike == bike:
+                    self.inventory[bike] += 1
+    
+            
+                
+class Customer(object):
     def __init__(self, name, budget):
         self.name = name
         self.budget = budget
     
     def buybike(self, purchased_bike, bikeshop):
-        purchase_price = purchased_bike.prodcost*(1+bikeshop.margin)
-        self.budget -= purchase_price
-        bikeshop.profit += bikeshop.margin*purchased_bike.prodcost
+        self.budget -= purchased_bike.price
+        bikeshop.profit += bikeshop.margin*purchased_bike.prod_cost
         bikeshop.inventory[purchased_bike] -= 1
-        print("{}, you have purchased {} for ${}. You have ${} left to spend.".format(self.name, purchased_bike.name, purchase_price, self.budget))
-        print("The bikeshop, {}, has made ${} in profit and has the following bikes remaining:\n".format(bikeshop.name, bikeshop.profit))
+
+class Wheel(object):
+    def __init__(self, name, weight, prod_cost, wheel_type):
+        self.name = name
+        self.weight = weight
+        self.prod_cost = prod_cost
+        self.wheel_type = wheel_type
+    
+    #how to ensure that there is a total of 3 different wheel_types? is this in a different function outside the class?
+
+class Frame(object):
+    def __init__(self, weight, prod_cost):
+        self.weight = weight
+        self.prod_cost = prod_cost
+    
+    def what_material(self):
+        #is there an easier way to write this loop?
+        loop = True
+        while loop == True:
+            material = input("Is the frame made of aluminum, carbon or steel? Type A, C, or S. ")
+            if material.lower() == 'a':
+                self.material = 'Aluminum' 
+                loop = True 
+            elif material.lower() == 'c':
+                self.material = 'Carbon'
+                loop = True
+            elif material.lower() == 's':
+                self.material = 'Steel'
+                loop = True
+            else:
+                print("Please re-enter either A, C, or S.")
+                loop = False
+
+class Manufacturer(object):
+    def __init__(self, name, bike1, bike2, bike3, mfg_margin):
+        self.name = name
+        self.mfg_margin = mfg_margin
+        self.bike1 = bike1
+        self.bike2 = bike2
+        self.bike3 = bike3
         
-        for n in bikeshop.inventory:
-            print("{}: {}".format(n.name,bikeshop.inventory[n]))
+        
+                
+        
+
+
+
     
 
     
--- main.py
+++ main.py
@@ -1,37 +1,60 @@
-from bicycle import bicycle, bikeshop, customers
+from bicycle import Bicycle, Bikeshop, Customer, Wheel, Manufacturer, Frame
 
-#Create 6 bicycle models
-Rager = bicycle("Rager",10, 200)
-Air = bicycle("Air", 4, 800)
-Trusty = bicycle("Trusty", 15, 100)
-Dust = bicycle("Dust", 8, 400)
-Glider = bicycle("Glider", 5, 600)
-Roller = bicycle("Roller", 12, 1200)
+#Create 4 wheels
+sturdy = Wheel("Sturdy", 2, 100, "clincher")
+slick = Wheel("Slick", 1, 150, "clincher")
+modern = Wheel("Modern", 1, 150, "tubeless")
+supreme = Wheel("Supreme", 1.5, 125, "tublar")
 
-#Create 3 customers
-Agatha = customers("Agatha",200)
-Blake = customers("Blake",500)
-Joe = customers("Joe", 1000)
+#Create 2 frames
+frame1 = Frame(.5, 100)
+frame2 = Frame (.75, 75)
+
+#Create 2 manufacturers
+schwinn = Manufacturer("Schwinn", "Rager","Air","Trusty", .5)
+bianchi = Manufacturer("Bianchi", "Dust", "Glider", "Roller", .4)
+
+#Create 6 Bicycle models
+rager = Bicycle("Rager", sturdy, slick, frame1, schwinn) #should sturdy be in quotes?
+air = Bicycle("Air", modern, modern, frame1, schwinn)
+trusty = Bicycle("Trusty", supreme, supreme, frame2, schwinn)
+dust = Bicycle("Dust", sturdy, sturdy, frame2, bianchi)
+glider = Bicycle("Glider", slick, slick, frame2, bianchi)
+roller = Bicycle("Roller", supreme, supreme, frame1, bianchi)
+
+#Create 3 Customer
+agatha = Customer("Agatha",200)
+blake = Customer("Blake",500)
+joe = Customer("Joe", 1000)
 
 #Create bike shop with 6 models and 20% markup
-inventory_list = {Air: 1, Trusty: 3, Dust: 10, Rager:2, Glider: 5, Roller: 9}
-Mike = bikeshop("Mike", inventory_list, .2)
+inventory_list = {air: 1, trusty: 3, dust: 10, rager:2, glider: 5, roller: 9}
+mike = Bikeshop("Mike", inventory_list, .2)
 
 #Print initial inventory
-for n in Mike.inventory:
-    print("{}: {}".format(n.name,Mike.inventory[n]))
+for n in mike.inventory:
+    print("{}: {}".format(n.name, mike.inventory[n]))
 
 #Print the bikes that each customer can purchase 
-customer_list = [Agatha, Blake, Joe]
-for n in customer_list:
-    print("\n{}:".format(n.name))
-    for b in Mike.inventory:
-        if b.prodcost*(1+Mike.margin) <= n.budget:
-            print(b.name)
+customer_list = [agatha, blake, joe]
+for c in customer_list:
+    print(mike.afford_bikes(c))
+
+#Prints receipt each time a customer buys a bike
+def print_receipt(customer, bike, Bikeshop):
+    print("{}, you have purchased {} for ${}. You have ${} left to spend.".format(customer.name, bike.name, bike.price, customer.budget))
+    print("The Bikeshop, {}, has made ${} in profit and has the following bikes remaining:\n".format(Bikeshop.name, Bikeshop.profit))
+    for n in Bikeshop.inventory:
+        print("{}: {}".format(n.name,Bikeshop.inventory[n]))
 
 #Each customer buys a bike.  
-Agatha.buybike(Trusty, Mike)
-Blake.buybike(Rager, Mike)
-Joe.buybike(Glider, Mike)
+agatha.buybike(trusty, mike)
+print_receipt(agatha, trusty, mike)
+blake.buybike(rager, mike)
+print_receipt(blake, rager, mike)
+joe.buybike(glider, mike)
+print_receipt(joe, glider, mike)
 
+
+    
 
